6d16162ee41b58b5ad7294eec9a0da748c82213b
abi-seg <abirami1488@gmail.com>
2026-04-06 22:35:44 +0200

    Fix:Flake 8 corrections

--- lettings/views.py
+++ lettings/views.py
@@ -8,13 +8,13 @@ logger = logging.getLogger(__name__)
 def index(request):
 
     """Display the list of lettings."""
-    #raise Exception("Erreur de test 500")
+    # raise Exception("Erreur de test 500")
     logger.info("Affichage de la liste des locations")
     lettings_list = Letting.objects.all()
     context = {"lettings_list": lettings_list}
     return render(request, "lettings/index.html", context)
 
-    
+
 def letting(request, letting_id):
 
     """Display the details of a single letting"""
